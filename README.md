3390fe8780f3cd83ab86e2324a3689f1738c7e03
Cristiano Sobral <cssobral2013@gmail.com>
2024-12-07 01:03:01 -0300

    Update v1.0

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# Alba-Sans
-Is developed from Poppins
+# Alba Sans
+
+Is developed from Poppins fonts. Main changes are:
+
+ - True italics;
+ - Double-storey 'a' in Roman;
+ - Imported Vietnamese support from FZ Poppins (https://fontzin.com/poppins-viet-hoa/);
+ - Added tabular figures (0-9 only);
+ - Main glyphs are imported from Poppins v3.010a, with some imported from v3.200;
+ - And others.
---
+++ fonts/generate-woff-woff2.py
@@ -0,0 +1,333 @@
+from fontTools.ttLib import TTFont
+
+'''Generate WOFF from TTF or OTF font.'''
+
+srcPath  = './ttf/AlbaSans-Thin.ttf'
+woffPath = './web/AlbaSans-Thin.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/AlbaSans-ThinItalic.ttf'
+woffPath = './web/AlbaSans-ThinItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/AlbaSans-ExtraLight.ttf'
+woffPath = './web/AlbaSans-ExtraLight.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/AlbaSans-ExtraLightItalic.ttf'
+woffPath = './web/AlbaSans-ExtraLightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+    
+srcPath  = './ttf/AlbaSans-Light.ttf'
+woffPath = './web/AlbaSans-Light.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/AlbaSans-LightItalic.ttf'
+woffPath = './web/AlbaSans-LightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/AlbaSans-LightItalic.ttf'
+woffPath = './web/AlbaSans-LightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/AlbaSans-Regular.ttf'
+woffPath = './web/AlbaSans-Regular.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/AlbaSans-Italic.ttf'
+woffPath = './web/AlbaSans-Italic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/AlbaSans-Medium.ttf'
+woffPath = './web/AlbaSans-Medium.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/AlbaSans-MediumItalic.ttf'
+woffPath = './web/AlbaSans-MediumItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/AlbaSans-SemiBold.ttf'
+woffPath = './web/AlbaSans-SemiBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/AlbaSans-SemiBoldItalic.ttf'
+woffPath = './web/AlbaSans-SemiBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/AlbaSans-SemiBold.ttf'
+woffPath = './web/AlbaSans-SemiBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/AlbaSans-SemiBoldItalic.ttf'
+woffPath = './web/AlbaSans-SemiBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/AlbaSans-Bold.ttf'
+woffPath = './web/AlbaSans-Bold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/AlbaSans-BoldItalic.ttf'
+woffPath = './web/AlbaSans-BoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/AlbaSans-ExtraBold.ttf'
+woffPath = './web/AlbaSans-ExtraBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/AlbaSans-ExtraBoldItalic.ttf'
+woffPath = './web/AlbaSans-ExtraBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/AlbaSans-Black.ttf'
+woffPath = './web/AlbaSans-Black.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/AlbaSans-BlackItalic.ttf'
+woffPath = './web/AlbaSans-BlackItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+
+
+'''Generate WOFF2 from TTF or OTF font.'''
+
+srcPath  = './ttf/AlbaSans-Thin.ttf'
+woff2Path = './web/AlbaSans-Thin.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/AlbaSans-ThinItalic.ttf'
+woff2Path = './web/AlbaSans-ThinItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/AlbaSans-ExtraLight.ttf'
+woff2Path = './web/AlbaSans-ExtraLight.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/AlbaSans-ExtraLightItalic.ttf'
+woff2Path = './web/AlbaSans-ExtraLightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+    
+srcPath  = './ttf/AlbaSans-Light.ttf'
+woff2Path = './web/AlbaSans-Light.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/AlbaSans-LightItalic.ttf'
+woff2Path = './web/AlbaSans-LightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/AlbaSans-LightItalic.ttf'
+woff2Path = './web/AlbaSans-LightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/AlbaSans-Regular.ttf'
+woff2Path = './web/AlbaSans-Regular.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/AlbaSans-Italic.ttf'
+woff2Path = './web/AlbaSans-Italic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/AlbaSans-Medium.ttf'
+woff2Path = './web/AlbaSans-Medium.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/AlbaSans-MediumItalic.ttf'
+woff2Path = './web/AlbaSans-MediumItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/AlbaSans-SemiBold.ttf'
+woff2Path = './web/AlbaSans-SemiBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/AlbaSans-SemiBoldItalic.ttf'
+woff2Path = './web/AlbaSans-SemiBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/AlbaSans-SemiBold.ttf'
+woff2Path = './web/AlbaSans-SemiBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/AlbaSans-SemiBoldItalic.ttf'
+woff2Path = './web/AlbaSans-SemiBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/AlbaSans-Bold.ttf'
+woff2Path = './web/AlbaSans-Bold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/AlbaSans-BoldItalic.ttf'
+woff2Path = './web/AlbaSans-BoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/AlbaSans-ExtraBold.ttf'
+woff2Path = './web/AlbaSans-ExtraBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/AlbaSans-ExtraBoldItalic.ttf'
+woff2Path = './web/AlbaSans-ExtraBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/AlbaSans-Black.ttf'
+woff2Path = './web/AlbaSans-Black.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/AlbaSans-BlackItalic.ttf'
+woff2Path = './web/AlbaSans-BlackItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
